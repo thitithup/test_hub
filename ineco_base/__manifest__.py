e3d4d40726d2da833d7e374bf8c6b458fca6bd12
tititab <thitithup@gmail.com>
2021-01-07 15:20:37 +0700

    First Module

---
+++ ineco_base/__manifest__.py
@@ -0,0 +1,23 @@
+# -*- coding: utf-8 -*-
+# Part of Odoo. See LICENSE file for full copyright and licensing details.
+{
+    'name': 'INECO BASE MODULES',
+    'version': '0.1',
+    'summary': 'Base modules',
+    'sequence': 1000,
+    'description': """    
+    """,
+    'category': 'Demo',
+    'website': 'https://www.ineco.co.th',
+    'images': [],
+    'depends': ["base"],
+    'data': [
+    ],
+    'demo': [
+    ],
+    'qweb': [
+    ],
+    'installable': True,
+    'application': False,
+    'auto_install': False,
+}
